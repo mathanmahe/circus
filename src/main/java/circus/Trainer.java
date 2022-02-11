06805579b3461bb5aaace487b1845aa6ecde3277
Mathan <mathanmahe99@gmail.com>
2022-02-11 16:26:59 +0800

    Merge commit '66b4b1ff5b34fba9a7a4558c47a82f93915f12d1'
    * commit '66b4b1ff5b34fba9a7a4558c47a82f93915f12d1':
      Add Tiger; update tests
      Add new types of animals to the circus
      Add names to animals
      Improve code
    
    # Conflicts:
    #	src/main/java/Animal.java
    #	src/main/java/Bird.java
    #	src/main/java/circus/Trainer.java

--- src/main/java/circus/Trainer.java
+++ src/main/java/circus/Trainer.java
@@ -1,6 +1,13 @@
+package circus;
+
+import circus.animal.Animal;
+import circus.animal.Bird;
+import circus.animal.Duck;
+import circus.animal.Parrot;
+
 public class Trainer {
     public static void main(String[] args) {
-        Duck d = new Duck();
+        Duck d = new Duck("Donald");
         getToSpeak(d);
 
         Bird b = (Bird) d;  // upcasting
@@ -12,12 +19,17 @@ public class Trainer {
         Duck d2 = (Duck) a; // downcasting
         getToSpeak(d2);
 
+<<<<<<< HEAD:src/main/java/Trainer.java
         train(new Duck());
          train(new Parrot());
 
 //        Animal a2 = new Animal();
 //        Bird b2 = new Bird();
 
+=======
+        train(new Duck("Daisy"));
+        // train(new animal.Parrot());
+>>>>>>> 66b4b1ff5b34fba9a7a4558c47a82f93915f12d1:src/main/java/circus/Trainer.java
     }
 
     private static void getToSpeak(Animal animal) {
@@ -25,6 +37,7 @@ public class Trainer {
     }
 
     private static void train(Bird bird) {
+<<<<<<< HEAD:src/main/java/Trainer.java
         if( bird instanceof Duck) {
             Duck d = (Duck) bird;
             d.swim();
@@ -32,5 +45,11 @@ public class Trainer {
             System.out.println("I am not a duck and I am not a duck");
         }
 
+=======
+        if (bird instanceof Duck) {
+            Duck d = (Duck) bird;
+            d.swim();
+        }
+>>>>>>> 66b4b1ff5b34fba9a7a4558c47a82f93915f12d1:src/main/java/circus/Trainer.java
     }
 }
